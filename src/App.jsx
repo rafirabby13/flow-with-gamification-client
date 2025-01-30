d3e6a0c73687b97caf37fe0656b162be4553eb1f
rafi <rafiahmedrabby282@gmail.com>
2025-01-30 23:56:15 +0600

    worked on dynamic title and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Fade } from "react-awesome-reveal";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
+      <Helmet>
+        <title>Home | Flow with Gamification</title>
+      </Helmet>
       <div className="text-center">
-        <h1 className="text-5xl font-bold mb-4 text-[#526E48]">Welcome to Quiz Game</h1>
+        <h1>
+          <Fade
+            cascade
+            damping={1e-1}
+            className="text-5xl font-bold mb-4 text-[#526E48]"
+          >
+            Welcome to Quiz Game
+          </Fade>
+        </h1>
         <Link to="/quiz" className="btn bg-[#526E48] text-[#F5F5F5] text-2xl">
           Start Game
         </Link>
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useAllData from "../hooks/useAllData";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 const Quiz = () => {
   const [selectedId, setSelectedId] = useState(null);
   const [selectedQuestion, setSelectedQuestion] = useState({});
@@ -11,7 +12,7 @@ const Quiz = () => {
   // console.log(allData.questions);
   // let count = 3
   let questions = allData.questions;
-const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     if (questions) {
       // console.log(selectedQuestion);
@@ -21,56 +22,55 @@ const navigate = useNavigate()
   }, [count, questions, selectedQuestion]);
 
   const handleIncreate = () => {
+    if (!selectedId) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Must select an option!!",
+          });
+    }
     if (selectedId && count < 9) {
       setCount(count + 1);
       setSelectedId(null);
     }
     if (count == 9) {
-        if (mark < 5) {
-            Swal.fire({
-                position: "top-end",
-                icon: "warning",
-                title: `Your Obatained ${mark}`,
-                text: "Not so good. Study Hard!!",
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
-        else if (mark < 8) {
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: `Your Obatained ${mark}`,
-                text: "Good. But should study more..!!",
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
-        else if (mark < 10) {
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: `Your Obatained ${mark}`,
-                text: "Well done. Carry on.!!",
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
-        else{
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: `Your Obatained ${mark}`,
-                text: "Excellent , Very Good, Wow..",
-                showConfirmButton: false,
-                timer: 1500
-              });
-        }
+      if (mark < 5) {
+        Swal.fire({
+          icon: "warning",
+          title: `Your Obatained ${mark}`,
+          text: "Not so good. Study Hard!!",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      } else if (mark < 8) {
+        Swal.fire({
+          icon: "success",
+          title: `Your Obatained ${mark}`,
+          text: "Good. But should study more..!!",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      } else if (mark < 10) {
+        Swal.fire({
+          icon: "success",
+          title: `Your Obatained ${mark}`,
+          text: "Well done. Carry on.!!",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: `Your Obatained ${mark}`,
+          text: "Excellent , Very Good, Wow..",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      }
 
-        
       setCount(0);
       setMark(0);
-      navigate('/')
+      navigate("/");
     }
     const result = selectedQuestion.options?.find(
       (option) => option.id == selectedId
@@ -86,19 +86,22 @@ const navigate = useNavigate()
     setSelectedId(option.id);
   };
   return (
-    <div className="flex h-screen justify-center items-center ">
-      <div className="bg-white shadow-xl mx-20 p-10 w-full rounded-2xl border border-gray-200">
-        <h1 className="text-4xl font-bold text-gray-800 mb-6">
+    <div className="flex min-h-screen justify-center items-center ">
+        <Helmet>
+        <title>Quiz | Flow with Gamification</title>
+      </Helmet>
+      <div className="bg-white shadow-xl p-2 md:mx-20 md:p-10 w-full rounded-2xl border border-gray-200">
+        <h1 className="text-xl md:text-4xl font-bold text-gray-800 mb-6">
           {count + 1}. {selectedQuestion.description}
         </h1>
 
-        <div className="flex flex-wrap justify-between border-2 border-gray-300 p-6 rounded-lg bg-gray-50">
+        <div className="flex flex-wrap justify-between border-2 border-gray-300 p-2 md:p-6 rounded-lg bg-gray-50">
           {selectedQuestion.options?.map((option, index) => (
-            <ol
+            <div
               key={index}
-              className="flex items-center w-full md:w-1/2 space-x-3 mb-4"
+              className="flex items-center w-full md:w-1/2 mb-1  md:mb-4"
             >
-              <li className="flex items-center gap-3 bg-white p-4 rounded-lg shadow-sm w-full hover:bg-gray-100 transition " >
+              <p className="flex items-center gap-3 bg-white p-4 rounded-lg shadow-sm w-full hover:bg-gray-100 transition ">
                 <input
                   checked={selectedId == option.id}
                   onChange={() => handleChange(option)}
@@ -108,13 +111,13 @@ const navigate = useNavigate()
                 <span className="text-lg text-gray-700">
                   {option.description}
                 </span>
-              </li>
-            </ol>
+              </p>
+            </div>
           ))}
         </div>
 
         <button
-          className="btn bg-[#526E48] text-[#F5F5F5] text-2xl px-16 rounded-lg mt-6 shadow-md hover:bg-[#405836] transition duration-300"
+          className="btn bg-[#526E48] text-[#F5F5F5] md:text-2xl px-10 md:px-16 rounded-lg mt-6 shadow-md hover:bg-[#405836] transition duration-300"
           onClick={handleIncreate}
         >
           Next
